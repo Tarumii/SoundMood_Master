47c5d51a3526d040149c25a818c8914d87182407
Yanis HICHER <yhicher@gmail.com>
2025-05-16 00:35:10 +0200

    build error import fix

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "../styles/login.css";
+import "../styles/Login.css";
 import SplashScreen from "../components/splashscreen";
 
 const Login = () => {
